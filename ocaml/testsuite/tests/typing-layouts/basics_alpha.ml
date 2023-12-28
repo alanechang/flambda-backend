586dec8295baa3b8f9a31f9cd85af7c1fff3488e
alanechang <alanechang@janestreet.com>
2023-12-28 17:14:57 -0500

    error for unsupported sorts

--- ocaml/testsuite/tests/typing-layouts/basics_alpha.ml
+++ ocaml/testsuite/tests/typing-layouts/basics_alpha.ml
@@ -1005,12 +1005,11 @@ type t13 = t_void array
 
 let x13 (VV v) = [| v |];;
 [%%expect{|
-Line 1, characters 8-24:
+Line 1, characters 17-24:
 1 | let x13 (VV v) = [| v |];;
-            ^^^^^^^^^^^^^^^^
-Error: Non-value detected in [value_kind].
-       Please report this error to the Jane Street compilers team.
-       t_void has layout void, which is not a sublayout of value.
+                     ^^^^^^^
+Error: Type t_void has layout void.
+       Array expressions may not yet contain types of this layout.
 |}];;
 
 let x13 v =
@@ -1018,9 +1017,8 @@ let x13 v =
   | [| v |] -> VV v
   | _ -> assert false
 [%%expect{|
-Lines 1-4, characters 8-21:
-1 | ........v =
-2 |   match v with
+Lines 2-4, characters 2-21:
+2 | ..match v with
 3 |   | [| v |] -> VV v
 4 |   | _ -> assert false
 Error: Non-value detected in [value_kind].
